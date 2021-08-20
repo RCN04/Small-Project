6a3f5a95bdacc4342979c108e096a96dfa464b79
Hyperyon IV <ryon.nielsen@gmail.com>
2021-08-19 19:55:24 -0700

    initial project version

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const path = require('path')
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static('public'));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
+app.set('view engine', 'ejs');
+
+const routes = require('./routes');
+routes(app);
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+server.listen(port, () => {
+	console.log(`Listening on *:${port}...`);
+});
