fe29d259e30f28982be2a15e116fbfc91804cd76
Hyperyon IV <ryon.nielsen@gmail.com>
2021-08-26 19:49:43 -0700

    Added Header to Small-Project

--- routes.js
+++ routes.js
@@ -1,9 +1,22 @@
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-			res.render('pages/index');
+		res.render('pages/index', {
+			title: "Home | Small Project",
+			active: 'home'
+		});
 	});
 
 	app.get('/about', function(req, res) {
-			res.render('pages/about');
+		var facts = [
+			"Born in Utah",
+			"I used to be a snowboarding instructor",
+			"I have a dog named Lyla"
+		];
+
+		res.render('pages/about', {
+			title: "About | Small Project",
+			facts: facts,
+			active: 'about'
+		});
 	});
 }
